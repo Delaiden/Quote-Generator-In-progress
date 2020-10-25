7748923ab39f3794526a0c91c0a93c84100e10b8
Delaiden <73080805+Delaiden@users.noreply.github.com>
2020-10-26 00:29:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // let apiQuotes = [];
 
 // show new quote
-function newQuote(){
+
+function newQuote() {
     //vyber si random quote z apiquotes array
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+
+    // Check if Author field is blank and replace it with quote 'Unkown'
+
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    //Check quote length to determine styling
+
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
+
+
 
 //get Quote from api
 
@@ -20,6 +45,16 @@ function newQuote(){
         // Catch error Here      
     }
 } */
+
+// Tweet quote - pouzivaj backtick pod escapom ``
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On load
 //getQuote();
